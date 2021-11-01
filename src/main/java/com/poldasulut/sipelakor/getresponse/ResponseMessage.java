a164a7ddf2835c244be18da15d84b2ec4eba9921
dansiapa <48487205+dansiapa@users.noreply.github.com>
2021-11-01 08:54:37 +0800

    upload-files

---
+++ src/main/java/com/poldasulut/sipelakor/getresponse/ResponseMessage.java
@@ -0,0 +1,18 @@
+package com.poldasulut.sipelakor.getresponse;
+
+public class ResponseMessage {
+
+    private String message;
+
+    public ResponseMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/poldasulut/sipelakor/repository/FileStorageService.java
@@ -0,0 +1,20 @@
+package com.poldasulut.sipelakor.repository;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface FileStorageService {
+    public void init();
+
+    public void save(MultipartFile file);
+
+    public Resource load(String filename);
+
+    public void deleteAll();
+
+    public Stream<Path> loadAll();
+}
