bf9fa77a69c8d7a455c711b9ae27d421c53b521c
dansiapa <48487205+dansiapa@users.noreply.github.com>
2021-11-02 03:14:19 +0800

    upload-files

--- src/main/java/com/poldasulut/sipelakor/controller/FormLaporanController.java
+++ src/main/java/com/poldasulut/sipelakor/controller/FormLaporanController.java
@@ -91,8 +91,8 @@ public class FormLaporanController {
             message = "Uploaded the files successfully: " + fileNames;
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
         } catch (Exception e) {
-            message = "Fail to upload files!"+e.toString();
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            message = "Fail to upload files!!"+e.toString();
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message+e.toString()));
         }
     }
 
